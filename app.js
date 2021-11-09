6f7cd7f531a9d722b0db433f6ef70ce0f9bff1c8
Amara Stephens <amarastephens@Amaras-MacBook-Pro.local>
2021-11-09 13:06:30 -0600

    fin

--- app.js
+++ app.js
@@ -1,107 +1,102 @@
 $(document).ready(function () {
+    let sentences = [
+        'ten ate neite ate nee enet ite ate inet ent eate',
+        'Too ato too nOt enot one totA not anot tOO aNot',
+        'oat itain oat tain nate eate tea anne inant nean',
+        'itant eate anot eat nato inate eat anot tain eat',
+        'nee ene ate ite tent tiet ent ine ene ete ene ate'
+    ];
+    let sentenceIndex = 0; // which sentence the user is on at any time.
+    let letterIndex = 0; // current letter in current sentence
+    let numOfMistakes = 0;
+    let highlighterPosition = 15;
+    let startTime;
 
-    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-    let charNumber = 0
-    let sentenceIndex = 0
-    let currentLetter = sentences[sentenceIndex].substring(charNumber, charNumber + 1);
-    let yellowBlock = $("#yellow-block")
-    let counter = 17
-    let totalLetters = 0
-    let mistakes = 0
+    $("#keyboard-upper-container").toggle();
+    $("#sentence").text(sentences[sentenceIndex]);
+    $("#target-letter").text(sentences[sentenceIndex][letterIndex]);
 
+    $(document).one("keypress", function () {
+        startTime = Date.now();
+    });
 
-
-    $("#keyboard-upper-container").hide();
-    $("#sentence").text(sentences[sentenceIndex])
-    $("#target-letter").text(sentences[sentenceIndex][charNumber]);
-
-
-
-    $(document).keydown(function (event) {
-        totalLetters++
-        if (totalLetters == 1) {
-
-            startDate = new Date();
-            startTime = startDate.getTime();
-
-        }
-
-
-        if (event.key == sentences[sentenceIndex][charNumber]) {
-            yellowBlock.css("left", `${counter}px`)
-            charNumber++
-            let correct = $("<span>✔</span>");
-            $("span").css("color", "green")
-            $(correct).addClass('green');
-            $(correct).appendTo("#feedback");
-            $("#target-letter").empty()
-            $("#target-letter").text(sentences[sentenceIndex][charNumber]);
-        }
-
-        else {
-            let incorrect = $("<span>✗</span>");
-            $("span").css("color", "red")
-            $(incorrect).addClass('red');
-            $(incorrect).appendTo("#feedback");
-            mistakes++
-        }
-
-        if (charNumber === sentences[sentenceIndex].length) {
-            sentenceIndex++
-            charNumber = 0
-            counter = 17.2
-            $("#sentence").text(sentences[sentenceIndex])
-            $("#target-letter").text(sentences[sentenceIndex][charNumber]);
-        }
-
-        counter += 17
-
-
-
-        if (event.shiftKey) {
+    $(document).keydown(function (e) {
+        // Show upper keyboard and hide lower when shift is pressed
+        if (e.key == "Shift") {
             $("#keyboard-upper-container").show();
             $("#keyboard-lower-container").hide();
-        }
+        } else {
+            // highlight pressed key
+            const key = e.key.charCodeAt(0);
+            $("#" + key).css("background-color", "yellow");
 
-    })
+            // check if user typed correct character and append symbol if yes or no.
+            if (e.key == sentences[sentenceIndex][letterIndex]) {
+                $("#feedback").append("<span class='glyphicon glyphicon-ok'></span>");
+            } else {
+                $("#feedback").append("<span class='glyphicon glyphicon-remove'></span>");
 
+                numOfMistakes++
+            }
 
-    $(document).keypress(function (event) {
+            $("#yellow-block").css("left", (highlighterPosition += 15) + 'px');
 
-        $(`#${event.which}`).css("background-color", "yellow");
-    })
+            // increment letterIndex by one on move and append next letter
+            letterIndex++
+            if (sentences[sentenceIndex][letterIndex] == " ") {
+                $("#target-letter").text("space");
+            } else {
+                $("#target-letter").text(sentences[sentenceIndex][letterIndex]);
+            }
 
-    $(document).keyup(function (event) {
+            // check if sentence is complete.
+            if (sentences[sentenceIndex].length == letterIndex) {
+                sentenceIndex++;
+                letterIndex = 0;
+                highlighterPosition = 15;
 
-        $("#keyboard-upper-container").hide();
-        $("#keyboard-lower-container").show();
+                if (sentenceIndex == sentences.length) {
+                    let endTime = Date.now();
+                    let gameTimeinMS = endTime - startTime;
+                    let minutes = gameTimeinMS / 60000
+                    alert(calcWPM(minutes, numOfMistakes));
 
-        $(`#${event.key.charCodeAt(0)}`).css("background-color", "")
+                    setTimeout(function () {
+                        const playAgain = confirm("Play again?");
 
+                        if (playAgain) {
+                            window.location.reload();
+                        }
+                    }, 1000);
+                }
 
+                $("#yellow-block").css("left", highlighterPosition + 'px');
 
+                $("#target-letter").text(sentences[sentenceIndex][letterIndex]);
 
-        if (sentenceIndex === sentences.length) {
+                $("#feedback").empty();
 
-            let endDate = new Date();
-            let endTime = endDate.getTime();
-            let minutes = (endTime - startTime) / 60000;
-
-            wpm = Math.round(54 / minutes - 2 * mistakes);
-
-            let results = confirm("You type " + wpm + " words per minute. Would you like to try again?");
-
-            if (results == true) {
-
-                location.reload();
+                $("#sentence").text(sentences[sentenceIndex]);
             }
         }
+    });
 
+    $(document).keyup(function (e) {
+        // Show lower keyboard and hide upper when shift is released
+        if (e.key == "Shift") {
+            $("#keyboard-upper-container").hide();
+            $("#keyboard-lower-container").show();
+        } else {
+            // highlight pressed key
+            const key = e.key.charCodeAt(0);
+            $("#" + key).css("background-color", "#f5f5f5");
+        }
+    });
+});
 
-
-    })
-})
-
+function calcWPM(minutes, numOfMistakes) {
+    return Math.round(54 / minutes - 2 * numOfMistakes)
+}
 
 
 
